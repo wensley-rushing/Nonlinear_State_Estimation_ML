797d932311bafce590f06fb2a594cbf038dcc9fd
s163761 <s163761@student.dtu.dk>
2022-10-27 18:18:25 +0200

    GPy_Working

--- OpenSeesPy_Model_2_Steel_Frame_2D_Python/Testing-OtherModules.py
+++ OpenSeesPy_Model_2_Steel_Frame_2D_Python/Testing-OtherModules.py
@@ -21,8 +21,8 @@ X0 = np.random.uniform(-3.,3.,(20,1))
 Y0 = np.sin(X0) + np.random.randn(20,1)*0.05
 
 
-dim = 35
-N = 6
+dim = 2
+N = 1000
 X = np.random.rand(N,3*dim)
 
 
@@ -37,22 +37,23 @@ kernel += GPy.kern.RBF(input_dim=dim, variance=1., lengthscale=1., active_dims=l
 
 
 m = GPy.models.GPRegression(X,Y,kernel)
-m['.*Gaussian_noise.variance'] = 0.
+#m['.*Gaussian_noise.variance'] = 0.
 print(kernel)
 plt.matshow(kernel.K(X))
 plt.colorbar()
 print('GPy:', kernel.K(X))
 print()
 
-#m.optimize(messages=True)
+print('Start optimizing')
+m.optimize(messages=True)
 #m.optimize_restarts(num_restarts = 10)
 
 #print(kernel)
 
-
-kernel2  = 1.*np.exp(-1/(2*1.)* (distance_matrix(X[:,0*dim:1*dim-1],X[:,0*dim:1*dim-1],p=2)**2))
-kernel2 += 1.*np.exp(-1/(2*1.)* (distance_matrix(X[:,1*dim:2*dim-1],X[:,1*dim:2*dim-1],p=2)**2))
-kernel2 += 1.*np.exp(-1/(2*1.)* (distance_matrix(X[:,2*dim:3*dim-1],X[:,2*dim:3*dim-1],p=2)**2))
+sys.exit()
+kernel2  = 1.*np.exp(-1/(2*1.)* (distance_matrix(X[:,0*dim:1*dim],X[:,0*dim:1*dim],p=2)**2))
+kernel2 += 1.*np.exp(-1/(2*1.)* (distance_matrix(X[:,1*dim:2*dim],X[:,1*dim:2*dim],p=2)**2))
+kernel2 += 1.*np.exp(-1/(2*1.)* (distance_matrix(X[:,2*dim:3*dim],X[:,2*dim:3*dim],p=2)**2))
 plt.matshow(kernel2)
 plt.colorbar()
 
