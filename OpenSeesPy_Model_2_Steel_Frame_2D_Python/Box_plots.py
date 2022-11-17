03bbcb73d709356d50e1be07e4e168655dd41e83
Gabriele202277 <s202277@student.dtu.dk>
2022-11-17 11:35:44 +0100

    Test_10

--- OpenSeesPy_Model_2_Steel_Frame_2D_Python/Box_plots.py
+++ OpenSeesPy_Model_2_Steel_Frame_2D_Python/Box_plots.py
@@ -25,11 +25,14 @@ test_vec = [
             'Test 5\n20 high var GMs\nL=25 s=5\ntrain node [23]',
             'Test 6\n20 high var GMs\nL=10 s=3\ntrain node [23]',
             'Test 7\n20 high var GMs\nL=25 s=5\ntrain node [33]',
+            'Test 8\n20 high var GMs\nL=10 s=3\ntrain node [33]',
+            'Test 9\n10 high energy GMs\nL=25 s=5\ntrain node [23]',
+            'Test 10\n10 high error GMs\nL=25 s=5\ntrain node [23]'
             ]
 
 n_test = len(test_vec)
 
-nodes = [42]
+nodes = [22]
 
 for i in nodes: 
     
@@ -44,8 +47,12 @@ for i in nodes:
         
         df = pd.DataFrame(columns=[])
         
-        test = 'test_' + test_lab[5]   # test index ex. test_1, test_2 .....
-        
+        if test_lab[6] == '\n':
+            test = 'test_' + test_lab[5]   # test index ex. test_1, test_2 .....
+        else:
+            test = 'test_' + test_lab[5] + test_lab[6] 
+            
+                    
         file_path = os.path.join(data_directory, f'node_{i}', f'00_Error_{test}.pkl')
         
         df = pd.read_pickle(file_path)
@@ -60,20 +67,20 @@ for i in nodes:
             axes[0].boxplot(df.loc['TRAC'].values.reshape(-1,1), widths=0.25, positions=[j])# labels = test_lab)        
             axes[0].set_title('TRAC error',  y=1.05, fontweight="bold", fontsize = '12')
             axes[0].text(x=(j+0.5)/(len(test_vec)) , y=1, s=f"({round(df.loc['TRAC'].values.reshape(-1,1).mean(),2)})", 
-                         va='bottom', ha='center', transform = axes[0].transAxes, fontsize = '12')
+                          va='bottom', ha='center', transform = axes[0].transAxes, fontsize = '12')
             
             # SMSE boxplot
             axes[1].boxplot(df.loc['SMSE'].values.reshape(-1,1), widths=0.25, positions=[j]) #labels = test_lab)   
             axes[1].set_title('SMSE error',  y=1.05, fontweight="bold", fontsize = '12')
             axes[1].text(x=(j+0.5)/(len(test_vec)) , y=1, s=f"({round(df.loc['SMSE'].values.reshape(-1,1).mean(),2)})", 
-                         va='bottom', ha='center', transform = axes[1].transAxes, fontsize = '12')
+                          va='bottom', ha='center', transform = axes[1].transAxes, fontsize = '12')
            
             # RMSE boxplot
             
             axes[2].boxplot(df.loc['RMSE'].values.reshape(-1,1), widths=0.25, positions=[j]) #labels = test_lab)   
             axes[2].set_title('RMSE error',  y=1.05, fontweight="bold", fontsize = '12')
             axes[2].text(x=(j+0.5)/(len(test_vec)) , y=1, s=f"({round(df.loc['RMSE'].values.reshape(-1,1).mean(),2)})", 
-                         va='bottom', ha='center', transform = axes[2].transAxes, fontsize = '12')
+                          va='bottom', ha='center', transform = axes[2].transAxes, fontsize = '12')
             
         
         
