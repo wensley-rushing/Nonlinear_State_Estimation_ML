3d1eb303e81ed1700011a2784a89c75f5c236efb
Gabriele202277 <s202277@student.dtu.dk>
2022-11-21 09:16:51 +0100

    Test_5_NN_LN_NL

--- OpenSeesPy_Model_2_Steel_Frame_2D_Python/GPR_Model_GPy.py
+++ OpenSeesPy_Model_2_Steel_Frame_2D_Python/GPR_Model_GPy.py
@@ -108,7 +108,7 @@ folder_accs = r'output_files\ACCS'
 
 folder_structure = r'output_files'
 
-folder_figure_save = r'output_files\Testing\Test_NL'
+folder_figure_save = r'output_files\Testing\Test_NN_all_test'
 
 #%% Load Structure
 Structure = pd.read_pickle( os.path.join(folder_structure, '00_Structure.pkl') )
@@ -1236,12 +1236,14 @@ import random
 df_datasets = pd.read_pickle(folder_structure + '/00_EQ_List.pkl')    
 
 train_LN = 'N'
-test_LN = 'L'
+test_LN = 'N'
 
 if train_LN =='L':
-    load_IDs = int_to_str3(random.sample(df_datasets[23]['L'], k=10))
+    # load_IDs = int_to_str3(random.sample(df_datasets[23]['L'], k=10))
+    load_IDs = int_to_str3(df_datasets[23]['L'])
 elif train_LN =='N':
-    load_IDs = int_to_str3(random.sample(df_datasets[23]['N'], k=10))
+    # load_IDs = int_to_str3(random.sample(df_datasets[23]['N'], k=10))
+    load_IDs = int_to_str3(df_datasets[23]['N'])
     
       
         
