4bc6c4a8789edbd5d528c6a5180236523953a7dd
Gabriele202277 <s202277@student.dtu.dk>
2022-12-12 13:57:01 +0100

    Generate linear - nonlinear dataset NN

--- OpenSeesPy_Model_2_Steel_Frame_2D_Python/Combine_Index_Results.py
+++ OpenSeesPy_Model_2_Steel_Frame_2D_Python/Combine_Index_Results.py
@@ -13,7 +13,7 @@ import sys
 
 #%% Folder structure
 
-folder_lf_1 = r'output_files'
+folder_lf_1 = r'output_files'  
 folder_lf_0 = r'output_files_LF_0'
 folder_lf_2 = r'output_files_LF_2'
 # folder_lf_2C = r'output_files_Conv'
@@ -47,26 +47,52 @@ df_Results_2 = pd.read_pickle(os.path.join(folder_lf_2, '00_Index_Results.pkl'))
 
 #%% Merge so all GMs are converged
 
-if False:
-    # Remove non-converged GM in original df
-    df_Results_2.drop(644, inplace=True)
+# if False:
+#     # Remove non-converged GM in original df
+#     df_Results_2.drop(644, inplace=True)
     
-    # Add converged GM and sort according to index
-    df_Results_2 = pd.concat([df_Results_2, df_Results_2C])
-    df_Results_2.sort_index(ascending=True, inplace=True)
+#     # Add converged GM and sort according to index
+#     df_Results_2 = pd.concat([df_Results_2, df_Results_2C])
+#     df_Results_2.sort_index(ascending=True, inplace=True)
     
-    # Save corrected df
-    df_Results_2.to_pickle(os.path.join(folder_lf_2, '00_Index_Results.pkl'))
+#     # Save corrected df
+#     df_Results_2.to_pickle(os.path.join(folder_lf_2, '00_Index_Results.pkl'))
 
-sys.exit()
-#%% Merge Results
+# sys.exit()
+# #%% Merge Results
 
-frames = [df_Results_1, df_Results_0, df_Results_2]
-df_Results_Merged = pd.concat(frames)
+# frames = [df_Results_1, df_Results_0, df_Results_2]
+# df_Results_Merged = pd.concat(frames)
 
-df_Structure_Merged = df_Structure_1.copy()
+# df_Structure_Merged = df_Structure_1.copy()
 
-#%% Save pkl files
+# #%% Save pkl files
 
-df_Results_Merged.to_pickle(os.path.join(folder_merged, '00_Index_Results.pkl'))
-df_Structure_Merged.to_pickle(os.path.join(folder_merged, '00_Structure.pkl'))
+# df_Results_Merged.to_pickle(os.path.join(folder_merged, '00_Index_Results.pkl'))
+# df_Structure_Merged.to_pickle(os.path.join(folder_merged, '00_Structure.pkl'))
+
+#%%
+
+IDs_L = pd.DataFrame(columns = ['GM', 'Energy'])
+IDs_NL = pd.DataFrame(columns = ['GM', 'Energy'])
+
+for idx_0 in df_Results_0.index: # takes the ID of the GM in first dataset
+    
+    if df_Results_0.loc[idx_0, 'E - glob'] <= 2:
+        
+        idx_2 = idx_0 + 301
+        
+        if df_Results_0.loc[idx_0, 'Ground motion'] != df_Results_2.loc[idx_2, 'Ground motion']:
+            print('Error: the two GMs do not correspond! Check index')
+        
+        if df_Results_2.loc[idx_2, 'E - glob'] > 2:
+            
+            IDs_L.loc[idx_0] = df_Results_0.loc[idx_0, 'Ground motion'], df_Results_0.loc[idx_0, 'E - glob']
+            IDs_NL.loc[idx_2] = df_Results_2.loc[idx_2, 'Ground motion'], df_Results_2.loc[idx_2, 'E - glob']
+            
+
+
+IDs_L.to_pickle(os.path.join('output_NN', 'Linear' , 'Physical_inter','IDs_linear.pkl'))
+IDs_NL.to_pickle(os.path.join('output_NN', 'Linear' , 'Physical_inter','IDs_non_linear.pkl'))
+
+            
